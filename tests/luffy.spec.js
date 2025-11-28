b3df9283277c612d437463aaad0f4918c0dd962a
gnraj-git-hub <nprinith11@gmail.com>
2025-11-28 15:14:10 +0530

    Sample File

--- tests/luffy.spec.js
+++ tests/luffy.spec.js
@@ -2,3 +2,21 @@
 //Future Pirate King 
 //Zoro 
 //Sanji
+
+//git checkout -b "branch name" --> To create new branch name 
+
+//git branch
+
+//git push -u origin "Branch name" 
+
+//git checkout is used to move from one branch to another branch
+
+// -b is used to create a new branch 
+
+// git branch is used to see the available branches and then the current branch will be highlighted
+
+// git push -u origin test  - this command is used to push the local branch into the remote repository
+
+//git push origin "branch name" --> It is used to push the committed changes into the branch
+
+
