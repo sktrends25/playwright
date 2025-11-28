e73abbd1b925eb3f905f32b0bbd45d8d3ff6be43
anandamary96-sudo <anandamary96@gmail.com>
2025-11-28 15:00:29 +0530

    joke

--- tests/train.spec.js
+++ tests/train.spec.js
@@ -1,2 +1,8 @@
-//trsin ticket bookig
-//
+//train  ticket bookig testing file
+import { test, expect } from '@playwright/test';
+
+test.describe('Train Ticket Booking', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.confirmtkt.com');
+  });
+});
